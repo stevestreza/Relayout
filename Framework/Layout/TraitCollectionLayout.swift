2217d9c5c88b17e475c3ee802fee157d83b9acf8
Steve Streza <steve@stevestreza.com>
2016-09-10 19:58:47 -0700

    Doc

--- Framework/Layout/TraitCollectionLayout.swift
+++ Framework/Layout/TraitCollectionLayout.swift
@@ -4,6 +4,8 @@ import UIKit
 /**
  *    A LayingOut object that returns NSLayoutConstraint objects only when the rootView has certain
  *    traits in its UITraitCollection.
+ *
+ *    Not available on OS X.
  */
 public struct TraitCollectionLayout: LayingOut {
     /// The UITraitCollection to match traits against.
